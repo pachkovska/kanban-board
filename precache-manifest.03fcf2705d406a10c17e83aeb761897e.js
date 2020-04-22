1992c50939f19e2d2e6befdf9c3d5d17901cf2e8
pachkovska <pachkovska@gmail.com>
2020-04-21 18:21:58 -0700

    Updates

---
+++ precache-manifest.03fcf2705d406a10c17e83aeb761897e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cabfd48f8b43c3b3efd7a227f9a557",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "0ae1f95c5a93059cd8c5",
+    "url": "/kanban-board/static/css/2.99fb0006.chunk.css"
+  },
+  {
+    "revision": "0d604f2d9c204bed802d",
+    "url": "/kanban-board/static/css/main.df8bddab.chunk.css"
+  },
+  {
+    "revision": "0ae1f95c5a93059cd8c5",
+    "url": "/kanban-board/static/js/2.6e8b8b04.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kanban-board/static/js/2.6e8b8b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d604f2d9c204bed802d",
+    "url": "/kanban-board/static/js/main.d30d9f4b.chunk.js"
+  },
+  {
+    "revision": "151847355737b0df0a41",
+    "url": "/kanban-board/static/js/runtime-main.9173a9fd.js"
+  }
+]);
